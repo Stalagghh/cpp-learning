f0bb0399270ecbcb02edee4e7a5df162b49e3925
Stalagghh <101994665+Stalagghh@users.noreply.github.com>
2026-08-20 21:14:07 +0300

    trash

--- day04/loop.cpp
+++ day04/loop.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 
+int main(){
+    std::string s;
+    std::cin >> s;
+
+    if(s == "One" || "Один"){
+        std::cout << "OK" << std::endl;
+    }
+    else{
+        std::cout << "NOT OK" << std::endl;
+    }
+    return 0;
+}
